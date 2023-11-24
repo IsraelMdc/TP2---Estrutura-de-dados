222249258d81dbdecb58dc73373d996e6cb003af
Palestina <israelmcarmo@hotmail.com>
2023-11-24 13:36:47 -0300

    Lista de meliantes implementada

--- xray.c
+++ xray.c
@@ -1,12 +1,13 @@
 #include "xray.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 struct paciente
 {
     char nome[50];
-    int cpf;
+    char cpf[12];
     int idade;
     int id;
     int t_entrada;
@@ -21,28 +22,35 @@ struct pacientes
 };
 
 
+int gen_randint(int initial_number, int final_number)
+{
+    return initial_number + rand()%(final_number - initial_number + 1);
+}
+
 int gen_idade()
-{
-
-
-    return idade;
+{    
+    return gen_randint(0,100);
 };
 
-int gen_cpf()
+void gen_cpf(PACIENTE *pacient)
 {
-
-
-    return cpf;
+    char v[11];  
+    for (int i = 0; i < 11 ; i++)
+    {
+        v[i] = gen_randint('0','9');
+    }
+    v[11] = "\0";
+    strcpy(pacient->cpf, v);
 };
 
 
-PACIENTES *dv_create()
+PACIENTES *lista_meliantes()
 {
    PACIENTES *dv = (PACIENTES *)malloc(sizeof(PACIENTES)); // Allocate memory for the DynVec structure
 
    dv->n = 0;                                          // Initialize the number of elements to 0
    dv->n_max = 4;                                      // Allocate initially space for 4 elements
-   PACIENTE**meliantes = (float *)malloc(dv->n_max * sizeof(float)); // Allocate memory for the vector data
+   PACIENTE**meliantes = (PACIENTES **)malloc(dv->n_max * sizeof(PACIENTES)); // Allocate memory for the vector data
 
    return dv; // Return a pointer to the created dynamic vector
 }
@@ -52,15 +60,9 @@ PACIENTE * cria_meliante(int t_entrada)
     static int id = 1;
     PACIENTE *pacient = (PACIENTE *)malloc(sizeof(PACIENTE));
     pacient->idade = gen_idade();//func randint
-    pacient->cpf = gen_cpf();
+    gen_cpf(pacient);
     pacient->id = id++;
     pacient->t_entrada = t_entrada;
     pacient->t_saida = 0;
     return pacient;
-};
-
-PACIENTES* lista_meliantes()
-{
-    PACIENTES **lista_pacientes = (PACIENTE **)malloc(sizeof(PACIENTES));
-
 };
